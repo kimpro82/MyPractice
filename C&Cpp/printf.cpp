27f51ce18da86e98f0223418163a8a2aef238179
Kangrok Kim <kimpro@uebermensch.tech>
2022-04-27 00:12:12 +0900

    C/C++ : `printf()` format test with `%d` `%ld` `%lld` (#199)
    * Create printf.c
    * Create printf.cpp
    
    * Update README.md

---
+++ C&Cpp/printf.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    char text[] = "%d %ld %lld\n";
+    printf(text, __SCHAR_MAX__, __SCHAR_MAX__, __SCHAR_MAX__);
+    printf(text, __INT8_MAX__, __INT8_MAX__, __INT8_MAX__);
+    printf(text, __SHRT_MAX__, __SHRT_MAX__, __SHRT_MAX__);
+    printf(text, __INT16_MAX__, __INT16_MAX__, __INT16_MAX__);
+    printf(text, __INT_MAX__, __INT_MAX__, __INT_MAX__);
+    printf(text, __LONG_MAX__, __LONG_MAX__, __LONG_MAX__);
+    printf(text, __INT32_MAX__, __INT32_MAX__, __INT32_MAX__);
+    printf(text, __LONG_LONG_MAX__, __LONG_LONG_MAX__, __LONG_LONG_MAX__);
+    printf(text, __INT64_MAX__, __INT64_MAX__, __INT64_MAX__);
+
+    return 0;
+}
