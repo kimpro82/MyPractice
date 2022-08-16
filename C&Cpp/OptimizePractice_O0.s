f7684f70b644b5ea5f08a150c58fdb447f84e485
Kangrok Kim <kimpro@uebermensch.tech>
2022-08-16 18:38:40 +0900

    Upload 6 .s files

---
+++ C&Cpp/OptimizePractice_O0.s
@@ -0,0 +1,75 @@
+	.file	"OptimizePractice.c"
+	.text
+	.globl	_operate
+	.def	_operate;	.scl	2;	.type	32;	.endef
+_operate:
+LFB10:
+	.cfi_startproc
+	pushl	%ebp
+	.cfi_def_cfa_offset 8
+	.cfi_offset 5, -8
+	movl	%esp, %ebp
+	.cfi_def_cfa_register 5
+	movl	8(%ebp), %eax
+	andl	$1, %eax
+	testl	%eax, %eax
+	je	L3
+	movl	12(%ebp), %eax
+	movl	(%eax), %eax
+	leal	1(%eax), %edx
+	movl	12(%ebp), %eax
+	movl	%edx, (%eax)
+L3:
+	nop
+	popl	%ebp
+	.cfi_restore 5
+	.cfi_def_cfa 4, 4
+	ret
+	.cfi_endproc
+LFE10:
+	.def	___main;	.scl	2;	.type	32;	.endef
+	.section .rdata,"dr"
+LC0:
+	.ascii "%d\12\0"
+	.text
+	.globl	_main
+	.def	_main;	.scl	2;	.type	32;	.endef
+_main:
+LFB11:
+	.cfi_startproc
+	pushl	%ebp
+	.cfi_def_cfa_offset 8
+	.cfi_offset 5, -8
+	movl	%esp, %ebp
+	.cfi_def_cfa_register 5
+	andl	$-16, %esp
+	subl	$32, %esp
+	call	___main
+	movl	$0, 20(%esp)
+	leal	20(%esp), %eax
+	movl	%eax, 24(%esp)
+	movl	$0, 28(%esp)
+	jmp	L5
+L6:
+	movl	24(%esp), %eax
+	movl	%eax, 4(%esp)
+	movl	28(%esp), %eax
+	movl	%eax, (%esp)
+	call	_operate
+	addl	$1, 28(%esp)
+L5:
+	cmpl	$9, 28(%esp)
+	jle	L6
+	movl	20(%esp), %eax
+	movl	%eax, 4(%esp)
+	movl	$LC0, (%esp)
+	call	_printf
+	movl	$0, %eax
+	leave
+	.cfi_restore 5
+	.cfi_def_cfa 4, 4
+	ret
+	.cfi_endproc
+LFE11:
+	.ident	"GCC: (MinGW.org GCC-6.3.0-1) 6.3.0"
+	.def	_printf;	.scl	2;	.type	32;	.endef
