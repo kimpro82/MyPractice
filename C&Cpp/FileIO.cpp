7195fdc781dabba0f1a168fe8968a33088531781
Kangrok Kim <kimpro@uebermensch.tech>
2022-08-27 22:05:25 +0900

    Create FileIO.cpp

---
+++ C&Cpp/FileIO.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <fstream>
+#define endl '\n'
+
+using namespace std;
+
+int main()
+{
+    // Write file
+    ofstream ofs;
+    ofs.open("FileIO.txt", ios::out);           // ios::out : make a new empty file
+    ofs << "My wife is crazy." << endl;
+    ofs << "Really crazy." << endl;
+    ofs.close();
+
+    // Read file
+    ifstream ifs;
+    string line;
+    ifs.open("FileIO.txt", ios::in);            // ios::in : read-only
+    while(getline(ifs, line))
+    {
+        cout << line << endl;
+    }
+    ifs.close();
+
+    return 0;
+}
