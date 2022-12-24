ee7fc956cfa4090f52d978278b96554aeae06849
Kangrok Kim <kimpro@uebermensch.tech>
2022-12-25 01:02:16 +0900

    Update CrazySyntax.rb

--- Ruby/CrazySyntax.rb
+++ Ruby/CrazySyntax.rb
@@ -16,14 +16,14 @@ end
 
 # Comparison operator vs eql?
 
-puts 1 == 1.0                                       # true
-puts 1.eql?(1.0)                                    # false
+puts 1 == 1.0                                               # true
+puts 1.eql?(1.0)                                            # false
 
 
 # Range : .. vs ...
 
-rng1 = (1..10).to_a                                 # 1 ~ 10
-rng2 = (1...10).to_a                                # 1 ~  9
+rng1 = (1..10).to_a                                         # 1 ~ 10
+rng2 = (1...10).to_a                                        # 1 ~  9
 
 print rng1, "\n"
 print rng2, "\n"
@@ -33,13 +33,38 @@ print rng2, "\n"
 
 arr = [1, 2, 3]
 
-arr.reverse
+arr.reverse                                                 # not save
 print arr, "\n"
 
-arr.reverse!
+arr.reverse!                                                # save
 print arr, "\n"
 
 
-# Symbol : similar with Hash but immutable (and faster)
+# Symbol : similar with strings but immutable and faster
+
+str = "I"
+sym = :U
+str += "♥"
+# sym += "2"                                                # undefined method `+' for :U:Symbol (NoMethodError)
+print str
+print sym, "\n"
+
+
 # .times : an iterator for numbers
-# UDF : return without return
+
+10.times do
+    print "no"
+end
+print "\n"
+
+
+# UDF : return the last line's evaluated result without return
+
+def Say()
+    a = "I hate you"
+    b = "You make me crazy"
+    c = "I want to leave you, but"
+    d = "I love you"
+end
+
+puts Say()
